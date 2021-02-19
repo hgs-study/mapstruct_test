20a03714d6debe1fdd074de095d40e82d8dfe5d6
hgs-study <hgstudy_@naver.com>
2021-02-19 15:51:18 +0900

    mapstruct test

---
+++ src/main/java/com/test/mapstruct_test/MapstructTestApplication.java
@@ -0,0 +1,13 @@
+package com.test.mapstruct_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapstructTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapstructTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/test/mapstruct_test/MapstructTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.mapstruct_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapstructTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
